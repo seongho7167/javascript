0bc571fc13e3de32d75ec27ffd25f562913f4d6d
seongho7167 <seongho4511@naver.com>
2020-01-28 11:06:04 +0900

    create to ex2.js

---
+++ ex3.js
@@ -0,0 +1,11 @@
+let level = "A";
+switch(level){
+    case "A" :
+        console.log("VIP LEVEL");
+        break
+    case "B" : 
+        console.log("Basic LEVEL");
+        break
+    default :
+        console.log("Nothing");
+}
