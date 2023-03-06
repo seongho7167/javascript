5acf8581d9b1dbe8efb354ea910da27feeaad84c
seongho <seongho@corebizs-macbook-air-4.local>
2023-03-06 01:19:03 +0000

    asdasdsds

--- ex1.js
+++ ex1.js
@@ -5,22 +5,22 @@ if(true){
     var txt2 = "text2";
     console.log(txt2);
 }
-// 
-// let txt3 = "text3";
-// const txt4 = "yang";
+
+let txt3 = "text3";
+const txt4 = "yang";
+console.log(txt3);
+console.log(txt4);
+txt3 = "lee";
+console.log(txt3);
+txt3 = 500;
+// txt4 = "park";
 // console.log(txt3);
-// console.log(txt4);
-// txt3 = "lee";
-// console.log(txt3);
-// txt3 = 500;
-// // txt4 = "park";
-// // console.log(txt3);
-// console.log(txt4);
-// if(true){
-//     let txt5 = "yang";
-//     console.log(txt5);
-//     var txt6 = "kim";
-//     console.log(txt6);
-// }
-// console.log(txt5);
-// console.log(txt6);
+console.log(txt4);
+if(true){
+    let txt5 = "yang";
+    console.log(txt5);
+    var txt6 = "kim";
+    console.log(txt6);
+}
+console.log(txt5);
+console.log(txt6);
